41ac31993e9942c1157c6b08209e41d5df4b3c90
vipinsharma0999 <vipin.vi0333@gmail.com>
2026-03-14 16:01:34 +0530

    change with login penal

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as bodyParser from 'body-parser';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  
   app.useStaticAssets(join(process.cwd(), 'src'));
-
-  const bodyParser = require('body-parser');
-  app.use(bodyParser.urlencoded({ extended: true }));
-
+  app.use(bodyParser.urlencoded({ extended: true }) as never);
   await app.listen(3000);
 }
-bootstrap();
+void bootstrap();
